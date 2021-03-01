72d6b10d3d73ac8caeb628bdf769dc374aea17f4
TaimoorRao <taimoor.rao12@gmail.com>
2021-03-01 19:10:45 +0500

    Initial Commit - Tab Layout with Fragments - Working on Pickers

---
+++ app/src/main/java/com/example/task08/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.task08;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.widget.TableLayout;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+    private TabLayout tabLayout;
+    private TabItem tabItem1, tabItem2, tabItem3;
+    private ViewPager viewPager;
+    private PageAdapter pageAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = (TabLayout) findViewById(R.id.tablayout1);
+        tabItem1 = (TabItem) findViewById(R.id.tab1);
+        tabItem2 = (TabItem) findViewById(R.id.tab2);
+        tabItem3 = (TabItem) findViewById(R.id.tab3);
+        viewPager = (ViewPager) findViewById(R.id.view_pager);
+        pageAdapter = new PageAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+        viewPager.setAdapter(pageAdapter);
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+                if (tab.getPosition() == 0 || tab.getPosition() == 1 || tab.getPosition() == 2)
+                    pageAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+    }
+}
---
+++ app/src/main/java/com/example/task08/PageAdapter.java
@@ -0,0 +1,31 @@
+package com.example.task08;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class PageAdapter extends FragmentPagerAdapter {
+    private int tabCount;
+
+    public PageAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+        tabCount = behavior;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0: return new TabFragment1();
+            case 1: return new TabFragment2();
+            case 2: return new TabFragment3();
+            default: return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return tabCount;
+    }
+}
